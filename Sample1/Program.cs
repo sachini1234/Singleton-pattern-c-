3b512d46cbca691692ec4d4f9154a76813e70d71
sachini123 <sachinithilakshika1996@gmail.com>
2021-01-08 17:49:48 -0800

    save all files

---
+++ Sample1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            singletonObj fromSeller = singletonObj.GetInstance;
+            fromSeller.PrintDetails("This message from the seller");
+
+            singletonObj fromCustomer = singletonObj.GetInstance;
+            fromCustomer.PrintDetails("This second message from customer");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sample1/singletonObj.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample1
+{
+    public sealed class singletonObj
+    {
+        private static int counter = 0;
+        private static singletonObj instance = null;
+        public static singletonObj GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new singletonObj();
+                return instance;
+            }
+        }
+        private singletonObj()
+        {
+            counter++;
+            Console.WriteLine("Item No. " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
